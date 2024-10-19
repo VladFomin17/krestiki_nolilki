08cc0ff216018b099152a51eed29c922c020d86a
VladFomin17 <fominvladosik@gmail.com>
2024-10-19 12:09:09 +0300

    сделал возможность победить

---
+++ krestiki_nolilki/krestiki_nolilki.py
@@ -0,0 +1,21 @@
+def initialize_board():
+    return [' ' for i in range(9)]  
+
+def print_board(board):
+    print("    1 | 2 | 3")
+    print(" -----------")
+    for i in range(3):
+        print(f"{i + 1} | {' | '.join(board[i * 3:i * 3 + 3])}")
+        print(" -----------")
+
+def check_winner(board):
+    
+    winning_combinations = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  
+        (0, 4, 8), (2, 4, 6)             
+    ]
+    for a, b, c in winning_combinations:
+        if board[a] == board[b] == board[c] != ' ':
+            return board[a] 
+    return None
